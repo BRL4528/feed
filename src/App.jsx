e61d4b27c3066303b010817b418146dd4c98d0fc
brl4528 <blgc.sgo@hotmail.com>
2022-08-23 05:30:15 -0400

    create component header

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from './components/Header';
+import './global.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Header />
+    <h1>
+     Hello
+    </h1>
+    </>
+  )
+}
+
+export default App
