8aa07b2219255b75a42732b394a4db91abbaa098
brl4528 <blgc.sgo@hotmail.com>
2022-10-24 22:04:58 -0400

    create compoenent commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css'
+import { Trash, ThumbsUp } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/brl4528.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContext}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Bruno Luiz</strong>
+              <time title='11 de Maio ás 08:13h' dateTime="2022-05-11 08:13:30" >Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
