9d070aaae94dc96f7bacdd8f98c0e396a4843c91
brl4528 <blgc.sgo@hotmail.com>
2022-10-24 22:04:10 -0400

    update component sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,15 +11,18 @@ export function Sidebar() {
       src="https://images.unsplash.com/photo-1549605659-32d82da3a059?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
     
      <div className={styles.profile}>
+    <Avatar src="https://github.com/brl4528.png" />
+
       <strong>Bruno Luiz</strong>
       <span>Web Developer</span>
      </div>
 
-     <fotter>
+     <footer>
       <a href="#">
+        <PencilLine size={20} />
         Editar seu perfil
       </a>
-     </fotter>
+     </footer>
     </aside>
     )
 }
